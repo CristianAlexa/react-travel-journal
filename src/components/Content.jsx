1b9a2d81c906bbd6d35660af1c400ad30c3d3723
Cristian Alexa <94307268+CristianAlexa@users.noreply.github.com>
2024-12-06 12:35:31 +0000

    Update Content.jsx

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -3,7 +3,6 @@ import "./Content.css";
 import data from "../assets/data/data";
 
 export default function Content() {
-  console.log(data[0]);
   const cards = data.map((place) => <Card {...place} />);
   return <section className="content">{cards}</section>;
 }
